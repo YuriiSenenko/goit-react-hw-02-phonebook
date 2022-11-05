6d78257dd40e8b2c62c9e549d87475f67334bfd7
YuriiSenenko <yurkosenenko@gmail.com>
2022-11-05 21:01:02 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,96 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import css from './App.module.css';
+
+import Title from './Title';
+import Form from './Form';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+export class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  // Передає пропс в компонент форми
+  formSubmitHandler = (contact, id) => {
+    contact.id = id;
+
+    this.setState({
+      contacts: [...this.state.contacts, contact],
+    });
+  };
+
+  // formSubmitHandler = (contact, id) => {
+  //   contact.id = id;
+  //   this.state.contacts.map(searchContact => {
+  //     if (searchContact.name === contact.name) {
+  //       alert(`${contact.name} is already in contacts`);
+  //     } else {
+  //       console.log('cerf');
+  //       this.setState({
+  //         contacts: [...this.state.contacts, contact],
+  //       });
+  //     }
+  //   });
+  // };
+
+  searchDublicate = contact => {};
+
+  onDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContact = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  renderContacts = contacts => {
+    return contacts.map(contact => (
+      <li className={css.contact} key={contact.id}>
+        {contact.name}: {contact.number}
+        <button
+          className={css.deleteBtn}
+          onClick={() => this.onDeleteContact(contact.id)}
+        >
+          Delete
+        </button>
+      </li>
+    ));
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const visibleContacts = this.getVisibleContact();
+
+    return (
+      <div>
+        <Title>Phonebook</Title>
+        <Form onSubmit={this.formSubmitHandler} />
+
+        <h2> Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          renderContacts={this.renderContacts}
+          visibleContacts={visibleContacts}
+        />
+      </div>
+    );
+  }
+}
